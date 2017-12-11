a24e76eaa6db134ebffbd771933d13d3437464d1
Stefan Mansson <st.mansson@icloud.com>
2017-12-11 20:19:42 +0200

    chore: Added GA

--- client/routes/HomeRoute.js
+++ client/routes/HomeRoute.js
@@ -5,14 +5,15 @@ import Timeline from '../containers/Timeline';
 import Preview from '../containers/Preview';
 import About from '../containers/About';
 import ErrorPage from '../containers/ErrorPage';
+import tracker from './../modules/tracker';
 
 const HomeRoute = () => (
     <div>
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/timeline/:date?" component={Timeline} />
-        <Route exact path="/s/:id" component={Preview} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/404" component={ErrorPage} />
+        <Route exact path="/" component={tracker(Homepage)} />
+        <Route exact path="/timeline/:date?" component={tracker(Timeline)} />
+        <Route exact path="/s/:id" component={tracker(Preview)} />
+        <Route exact path="/about" component={tracker(About)} />
+        <Route exact path="/404" component={tracker(ErrorPage)} />
     </div>
 );
 
